61af9dd53ebfad5d676805198f646fdf2be141e1
tecle@126.com <tecle@126.com>
2017-09-08 10:16:03 +0800

    remove debug param;  use better way to update live data.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from __future__ import absolute_import, unicode_literals
 import os
 import sys
 import json
-import time
 import signal
 import logging
 import tornado.gen
@@ -22,45 +21,29 @@ class ScannerFactory(object):
     def __init__(self, conf_path):
         obj = json.load(open(conf_path))
         self.scanners = [CommonScanner(k, v) for k, v in obj.items()]
-        self.last_update = 0
         self.cache_time = 60
         self.anchors = []
-        self.wake_interval = 120 * 1000
+        self.wake_interval = 10
         self._timer = None
 
-    def _expired(self):
-        return time.time() - self.last_update > self.cache_time
-
-    def start_timer_task(self):
-        self._timer = tornado.ioloop.PeriodicCallback(self.update_async, self.wake_interval)
-        self._timer.start()
-
-    def stop_timer_task(self):
-        self._timer.stop()
-
-    def update(self):
-        if self._expired():
-            self.anchors = []
-            for p in self.scanners:
-                p.crawl()
-                self.anchors += p.anchors
-            self.last_update = time.time()
+    def start_update_job(self):
+        tornado.ioloop.IOLoop.current().spawn_callback(self.update_async)
 
     @tornado.gen.coroutine
     def update_async(self):
-        if self._expired():
-            logging.info('begin async update')
+        while True:
+            logging.info('begin async update.')
             self.anchors = []
             for p in self.scanners:
                 yield p.crawl_async()
                 self.anchors += p.anchors
-            self.last_update = time.time()
+            logging.info('async update over.')
+            yield tornado.gen.sleep(self.wake_interval)
 
 
-def register_signal(scanner_factory):
+def register_signal():
     def stop_server(signum, frame):
         logging.info('%s, %s', signum, frame)
-        scanner_factory.stop_timer_task()
         tornado.ioloop.IOLoop.current().stop()
 
     signal.signal(signal.SIGQUIT, stop_server)
@@ -71,11 +54,9 @@ def register_signal(scanner_factory):
 def start_server(port):
     port = int(port)
     sf = ScannerFactory('conf/tv.json')
-    sf.update()
     settings = {
         'static_path': os.path.join(CUR, 'views/static'),
         'static_url_prefix': 's',
-        'debug': 'false',
         'template_path': os.path.join(CUR, 'views/dynamic'),
         'scanner': sf
     }
@@ -86,8 +67,8 @@ def start_server(port):
     )
 
     app.listen(port)
-    sf.start_timer_task()
-    register_signal(sf)
+    sf.start_update_job()
+    register_signal()
     ioloop = tornado.ioloop.IOLoop.current()
     logging.info('server listen on port: %s', port)
     ioloop.start()
